9df0815e99389445d5a46f8d4b128b1603957ced
MarkoMozina <marko.mozina1@gmail.com>
2024-05-24 23:27:08 +0200

    deleted code

--- notebooks/makedirs.py
+++
@@ -1,8 +0,0 @@
-import os
-
-names = ["bert-base-multilingual-uncased", "all-MiniLM-L12-v2", "distiluse-base-multilingual-cased-v2", "distilbert-base-uncased"]
-
-
-for name in names:
-    os.makedirs(f"models/{name}", exist_ok=True)
-    os.makedirs(f"models/tsdae-{name}", exist_ok=True)
